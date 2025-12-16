ccbacfa6f096c625c333fc5f6242a29424c8c35c
Williams Anthony <willay.anthony@gmail.com>
2025-12-16 14:53:27 +0100

    Bug fixes

--- ms-orders/src/main/java/com/orders/infrastructure/client/ProductClient.java
+++ ms-orders/src/main/java/com/orders/infrastructure/client/ProductClient.java
@@ -2,6 +2,7 @@ package com.orders.infrastructure.client;
 
 import com.orders.application.dto.OrderedProductDTO;
 import com.orders.infrastructure.exception.ExternalServiceDownException;
+import com.orders.infrastructure.exception.ResourceNotFoundException;
 import com.orders.infrastructure.health.ExternalServiceChecker;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -18,9 +19,24 @@ public class ProductClient {
     private final WebClient webClient;
     private final ExternalServiceChecker externalServiceChecker;
 
+
+    public boolean productNotExist(Long productId) {
+        return !Boolean.TRUE.equals(webClient.get()
+                .uri(PRODUCT_SERVICE_API + "/{id}", productId)
+                .retrieve()
+                .toBodilessEntity()
+                .map(res -> res.getStatusCode().is2xxSuccessful())
+                .onErrorReturn(false)
+                .block());
+    }
+
+
     public OrderedProductDTO getProduct(Long productId) {
         if (!externalServiceChecker.checkService(BASE_PRODUCT_SERVICE_URL)) {
             throw new ExternalServiceDownException("product-service", "productId", productId.toString());
+        }
+        if (productNotExist(productId)) {
+            throw new ResourceNotFoundException("Product", "id", productId);
         }
         return webClient.get()
                 .uri(PRODUCT_SERVICE_API + "/{id}", productId)
@@ -32,6 +48,9 @@ public class ProductClient {
     public OrderedProductDTO updateStock(Long productId, Integer quantity) {
         if (!externalServiceChecker.checkService(BASE_PRODUCT_SERVICE_URL)) {
             throw new ExternalServiceDownException("product-service", "productId", productId.toString());
+        }
+        if (productNotExist(productId)) {
+            throw new ResourceNotFoundException("Product", "id", productId);
         }
         Map<String, Integer> body = Map.of("quantity", quantity);
         return webClient.patch()
